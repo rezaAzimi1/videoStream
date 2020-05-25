791fd31e1d9c6c7da59c9d23952bd691f7c9da79
Reza Azimi <reza.azimi.234353637062@gmail.com>
2020-05-25 13:20:28 +0430

    refactored add webcam video stream

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const express =require("express");
 const app = express();
-const server =require("http").Server(app);
-const io=require("socket.io")(server);
-const FPS=10;
+const httpserver =require("http").Server(app);
+const socketIo=require("socket.io")(httpserver);
+//*************************file server section*****************************
+// use http://localhost:3000/student for viewing video stream
 app.get('/student',(req,res)=>{
     res.sendFile(__dirname+"/htmlpages/student.html");
 })
+// use http://localhost:3000/teacher and let the camera turn on for produce video stream  
 app.get('/teacher',(req,res)=>{
     res.sendFile(__dirname+"/htmlpages/teacher.html");
 })
-io.on('connection',function(socket){
+//*************************END file server section*****************************
+//*************************stream server section*****************************
+socketIo.on('connection',function(socket){
     socket.on('stream',function(image){
-        socket.broadcast.emit("image",image);
+        socket.broadcast.emit("streamimage",image);
     });
 });
+//*************************END stream server section*****************************
+// If you have changed the port and host, make a change in ./htmlpages/teacher.html & ./htmlpages/student.html
 const port=3000;
 const host="127.0.0.1" 
-server.listen(port,host);
+httpserver.listen(port,host);
